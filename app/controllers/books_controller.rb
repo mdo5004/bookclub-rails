c27d085a12880bf979381347963399ce2cd5f62e
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-17 14:06:33 -0400

    refactored

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,16 @@
 class BooksController < ApplicationController
+    
     def search
         test_search = "https://www.goodreads.com/search/index.xml?key=U19zPpjBZNZMUrsjpA&q=Ender%27s+Game"
-        @books = GoodreadsService.books({key: "U19zPpjBZNZMUrsjpA", title: "ender's game"})
+        
+        results = GoodreadsService.books({key: "U19zPpjBZNZMUrsjpA", title: "ender's game"})
+        
+        BookSearchResult.destroy_all
+        @books = results.collect do |result|
+            BookSearchResult.create(result)
+        end
         
         render json: @books
     end
+    
 end
--- app/services/goodreads_service.rb
+++ app/services/goodreads_service.rb
@@ -4,8 +4,8 @@ class GoodreadsService
         test_search= "https://www.goodreads.com/search/index.xml?key=U19zPpjBZNZMUrsjpA&q=Ender%27s+Game" 
         resp = Faraday.get(test_search) 
         xml_resp = Nokogiri::XML(resp.body)
-        xml_resp.css("work").each { |work| 
-            BookSearchResult.create({
+        results = xml_resp.css("work").collect { |work| 
+            {
                 author: work.css('author').css('name').text,
                 author_id: work.css('author').css('id').text,
                 title: work.css('title').text,
@@ -14,8 +14,8 @@ class GoodreadsService
                 average_rating: work.css('average_rating').text,
                 image_url: work.css('image_url').text,
                 small_image_url: work.css('small_image_url').text                
-            })
+            }
         }
-        return BookSearchResult.all
+        return results
     end
 end
