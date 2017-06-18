c503ff00f21449656cef8324808c0c10d7b6fa47
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-18 17:13:10 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Comment.create([{ username: "mdo5004", page: 111, text: "This is a good book", book_id: 181},
+{ username: "rvo1987", page: 222, text: "No it's not.", book_id: 181},    
+{ username: "mdo5004", page: 111, text: "This is a good book", book_id: 181},
+{ username: "rvo1987", page: 222, text: "No it's not.", book_id: 181},    
+{ username: "mdo5004", page: 111, text: "This is a good book", book_id: 181},
+{ username: "rvo1987", page: 222, text: "No it's not.", book_id: 181},    
+{ username: "mdo5004", page: 111, text: "This is a good book", book_id: 181},
+{ username: "rvo1987", page: 222, text: "No it's not.", book_id: 181},    
+{ username: "mdo5004", page: 111, text: "This is a good book", book_id: 181},
+{ username: "rvo1987", page: 222, text: "No it's not.", book_id: 181},    
+{ username: "mdo5004", page: 111, text: "This is a good book", book_id: 181},
+{ username: "rvo1987", page: 222, text: "No it's not.", book_id: 181},    
+{ username: "mdo5004", page: 111, text: "This is a good book", book_id: 181},
+{ username: "rvo1987", page: 222, text: "No it's not.", book_id: 181},    
+{ username: "mdo5004", page: 111, text: "This is a good book", book_id: 181},
+{ username: "rvo1987", page: 222, text: "No it's not.", book_id: 181},    
+{ username: "mdo5004", page: 111, text: "This is a good book", book_id: 181},
+{ username: "rvo1987", page: 222, text: "No it's not.", book_id: 181},    
+{ username: "mdo5004", page: 111, text: "This is not a good book", book_id: 221},
+{ username: "rvo1987", page: 222, text: "Yes it is.", book_id: 221}])
