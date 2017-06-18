20b854e46538f2e1d1a93a30e8de832c06486255
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-18 17:11:35 -0400

    index method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ApplicationController
     def index
         if params[:book_id]
-            
+            book = Book.find_by(book_id: params[:book_id])
+            @comments = book.comments
         else
-            
+            @comments = Comment.all
         end
+        render json: @comments
     end
     def create
         
