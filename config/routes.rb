2768527b8490069d468e7b068053d17928310529
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-19 11:52:03 -0400

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     
     get '/search', to: "books#search"
-    resources :book do
+    resources :books do
         resources :comments, only: [:index, :create]
     end
     # I think I should set up Action Cables to update comments in real time
