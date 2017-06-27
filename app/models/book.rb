58a07ad0a3936722f28b1f36776a6275f9fe7ff9
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-27 12:52:38 -0400

    done

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ApplicationRecord
 #    belongs_to :author
     has_many :comments
+    has_many :user_books
+    has_many :readers, through: :user_books, foreign_key: "user_id"
     
     def self.visible
         Book.where("visible = ?", true)
